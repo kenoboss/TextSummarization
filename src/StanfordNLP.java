1724b22dc04ac8acdf25e98b1fded666f983e75d
Kenoboss <tziegelmayer@web.de>
2018-01-18 12:41:26 +0100

    tokenizer

--- src/StanfordNLP.java
+++ src/StanfordNLP.java
@@ -84,6 +84,26 @@ public class StanfordNLP {
 		}
 		return result;
 	}
+
+
+	public List<List<String>> stanfordSentenceTokenizer (String inputText){
+		List<List<String>> result = new ArrayList<>();
+		Properties props = new Properties();
+		props.put("annotators", "tokenize, ssplit, pos, lemma");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+		Annotation document = new Annotation(inputText);
+		pipeline.annotate(document);
+		List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+		for(CoreMap sentence: sentences) {
+			List<String> sentenceList = new ArrayList<>();
+			for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+				String word = token.get(TextAnnotation.class);
+				sentenceList.add(word);
+			}
+			result.add(sentenceList);
+		}
+		return result;
+	}
 	
 	
 	public static void main (String [] args) {
