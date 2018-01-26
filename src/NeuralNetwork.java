b864ad37b9559b8cd3b0c1645066355fd9db163a
siwer <kypsiw@web.de>
2018-01-26 15:55:19 +0100

    NeuralNetworkTest

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -1,5 +1,6 @@
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
 import org.datavec.api.util.ClassPathResource;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
@@ -8,7 +9,6 @@ import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration.ListBuilder;
-import org.deeplearning4j.nn.conf.Updater;
 import org.deeplearning4j.nn.conf.distribution.UniformDistribution;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
@@ -20,46 +20,96 @@ import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
-import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
-
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 public class NeuralNetwork {
-    int seed = 50;
-    double learningRate = 0.005;
-    int batchSize = 50;
-    int nEpochs = 100;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int batchSize = 50;
+        Helper reader = new Helper();
+        String s = System.getProperty("user.dir");
+        final String filenameTrain  = new ClassPathResource("/home/wern/Uni/Master/3._Semester/Projektseminar/TextSummarization/data/temp/trainingsSetSmall.csv").getFile().getPath();
+        final String filenameTest  = new ClassPathResource("/home/wern/Uni/Master/3._Semester/Projektseminar/TextSummarization/data/temp/trainingsSetSmall.csv").getFile().getPath();
 
-    //Da wir 5 Features haben
-    int numInputs = 5;
-    //Satz kommt in Zusammenfassung oder nicht
-    int numOutputs = 2;
-    int numHiddenNodes = 50;
+        RecordReader rr = new CSVRecordReader();
+        //rr.initialize(new FileSplit(new File("src/main/resources/classification/linear_data_train.csv")));
+        rr.initialize(new FileSplit(new File(filenameTrain)));
+        DataSetIterator trainIter = new RecordReaderDataSetIterator(rr,batchSize,0,2);
 
-    //Overwrite for our Structure
-    //.csv => Label (0|1), Vektor (...)
-    // 50/50 Training, Test
-    //final String filenameTrain  = new ClassPathResource("UNSERE TRAININGSDATEN").getFile().getPath();
-    //final String filenameTest  = new ClassPathResource("UNSERE TESTDATEN").getFile().getPath();
+        RecordReader rrTest = new CSVRecordReader();
+        rrTest.initialize(new FileSplit(new File(filenameTest)));
+        DataSetIterator testIter = new RecordReaderDataSetIterator(rrTest,batchSize,0,2);
 
+        // Set up network configuration
+        NeuralNetConfiguration.Builder builder = new NeuralNetConfiguration.Builder();
+        builder.iterations(100);
+        builder.learningRate(0.1);
+        builder.seed(123);
+        builder.useDropConnect(false);
+        builder.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT);
+        builder.biasInit(0);
+        builder.miniBatch(false);
 
-    MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-            .seed(seed)
-            .iterations(1)
-            .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-            .learningRate(learningRate)
-            .updater(Updater.NESTEROVS)
-            .list()
-            .layer(0, new DenseLayer.Builder().nIn(numInputs).nOut(numHiddenNodes)
-                    .weightInit(WeightInit.XAVIER)
-                    .activation(Activation.RELU)
-                    .build())
-            .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                    .weightInit(WeightInit.XAVIER)
-                    .activation(Activation.SOFTMAX)
-                    .nIn(numHiddenNodes).nOut(numOutputs).build())
-            .pretrain(false).backprop(true).build();
+        // create a multilayer network with 2 layers (including the output
+        // layer, excluding the input payer)
+        ListBuilder listBuilder = builder.list();
+        DenseLayer.Builder hiddenLayerBuilder = new DenseLayer.Builder();
+        // two input connections - simultaneously defines the number of input
+        // neurons, because it's the first non-input-layer
+        hiddenLayerBuilder.nIn(5);
+        // number of outgooing connections, nOut simultaneously defines the
+        // number of neurons in this layer
+        hiddenLayerBuilder.nOut(20);
+        // put the output through the sigmoid function, to cap the output
+        // valuebetween 0 and 1
+        hiddenLayerBuilder.activation(Activation.SIGMOID);
+        // random initialize weights with values between 0 and 1
+        hiddenLayerBuilder.weightInit(WeightInit.DISTRIBUTION);
+        hiddenLayerBuilder.dist(new UniformDistribution(0, 1));
+        // build and set as layer 0
+        listBuilder.layer(0, hiddenLayerBuilder.build());
+        Builder outputLayerBuilder = new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD);
+        outputLayerBuilder.nIn(20);
+        outputLayerBuilder.nOut(2);
+        outputLayerBuilder.activation(Activation.SOFTMAX);
+        outputLayerBuilder.weightInit(WeightInit.DISTRIBUTION);
+        outputLayerBuilder.dist(new UniformDistribution(0, 1));
+        listBuilder.layer(1, outputLayerBuilder.build());
 
+        listBuilder.pretrain(false);
+        listBuilder.backprop(true);
+        // build and init the network, will check if everything is configured
+        // correct
+        MultiLayerConfiguration conf = listBuilder.build();
+        MultiLayerNetwork net = new MultiLayerNetwork(conf);
+        net.init();
+        // add an listener which outputs the error every 100 parameter updates
+        net.setListeners(new ScoreIterationListener(100));
+        // C&P from GravesLSTMCharModellingExample
+        // Print the number of parameters in the network (and for each layer)
+        Layer[] layers = net.getLayers();
+        int totalNumParams = 0;
+        for (int i = 0; i < layers.length; i++) {
+            int nParams = layers[i].numParams();
+            System.out.println("Number of parameters in layer " + i + ": " + nParams);
+            totalNumParams += nParams;
+        }
+        System.out.println("Total number of network parameters: " + totalNumParams);
+        // here the actual learning takes place
+        net.fit(trainIter);
+        // create output for every training sample
+        System.out.println("Evaluate model....");
+        Evaluation eval = new Evaluation(2);
+        while(testIter.hasNext()){
+            DataSet t = testIter.next();
+            INDArray features = t.getFeatureMatrix();
+            INDArray lables = t.getLabels();
+            INDArray predicted = net.output(features,false);
+            eval.eval(lables, predicted);
+        }
+        //Print the evaluation statistics
+        System.out.println(eval.stats());
+    }
 }
