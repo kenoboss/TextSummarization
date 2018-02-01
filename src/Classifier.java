172618efb50a77fa9817aff484dd69fa8a937393
siwer <kypsiw@web.de>
2018-02-01 21:33:56 +0100

    Classifier init

---
+++ src/Classifier.java
@@ -0,0 +1,31 @@
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.util.ClassPathResource;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Classifier {
+    public MultiLayerNetwork loadNet (String pathToNet) throws IOException {
+        return ModelSerializer.restoreMultiLayerNetwork(pathToNet);
+    }
+
+    public String summarize (MultiLayerNetwork trainedNet,String text) {
+        //To Do: process text to Feature Vectors, Classify Feature Vectors, Add to Summary if Classified as 1
+        String summary = "";
+        //Use Method "predict" on trainedNet to classify
+        return summary;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Classifier test = new Classifier();
+        MultiLayerNetwork restored = test.loadNet("trainedNetwork.zip");
+        System.out.println(restored.params());
+    }
+}
