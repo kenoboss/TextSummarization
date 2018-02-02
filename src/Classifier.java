b80fd61e7d286ca5417075aeb3aaa058c8a47bf0
siwer <kypsiw@web.de>
2018-02-02 09:40:44 +0100

    update Classifier

--- src/Classifier.java
+++ src/Classifier.java
@@ -5,8 +5,11 @@ import org.datavec.api.util.ClassPathResource;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.factory.Nd4j;
+
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
@@ -33,10 +36,17 @@ public class Classifier {
         MultiLayerNetwork restored = test.loadNet("trainedNetwork.zip");
         System.out.println(restored.params());
 
-        String filename = new ClassPathResource("/data/temp/classificationTest.csv").getFile().getPath();
-        DataSet test = readCSVDataset(filename,
-                50, 5, 2);
+        //String filenameTest = new ClassPathResource("/data/temp/classificationTest.csv").getFile().getPath();
 
-        restored.feedForward();
+        RecordReader recordReader = new CSVRecordReader(0,",");
+        recordReader.initialize(new FileSplit(new ClassPathResource("/data/temp/classificationTest.csv").getFile()));
+        DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader,4);
+        DataSet next = iterator.next();
+        // Customizing params
+        Nd4j.MAX_SLICES_TO_PRINT = 10;
+        Nd4j.MAX_ELEMENTS_PER_SLICE = 10;
+
+        restored.predict(next);
+
     }
 }
