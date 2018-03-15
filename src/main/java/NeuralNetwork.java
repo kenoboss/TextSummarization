384b91e37a23011ba26162720c084a983a094bca
Kenoboss <tziegelmayer@web.de>
2018-03-15 16:28:29 +0100

    update trainingsNetwork

--- src/main/java/NeuralNetwork.java
+++ src/main/java/NeuralNetwork.java
@@ -24,72 +24,78 @@ import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
 /**
- * @author Simon Werner
+ * @author Simon Werner and Tobias Ziegelmayer
  * @version 1.0.0
  * This class contains a main method that creates, trains, evaluates and saves a neural network with the given parameters
  */
 
 public class NeuralNetwork {
+    private static final int NUMFEATUREVECTORSIZE = 5;
+    private static final int NUMHIDDENNODES = 10;
+    private static final int NUMFINALCLASSES = 2;
+    private static final String HOME_PATH = "/home/ziegelmayer/textsummarisation/";
+
+    static HashMap<String, List<Double>> results = new HashMap<>();
+
+
     public static void main(String[] args) throws IOException, InterruptedException {
+        WeightInit [] weightInits = {WeightInit.DISTRIBUTION, WeightInit.ZERO, WeightInit.SIGMOID_UNIFORM,
+                WeightInit.UNIFORM, WeightInit.XAVIER, WeightInit.XAVIER_UNIFORM, WeightInit.XAVIER_FAN_IN,
+                WeightInit.XAVIER_LEGACY, WeightInit.RELU, WeightInit.RELU_UNIFORM};
+
+        String [] activations = {"CUBE", "ELU", "HARDSIGMOID", "HARDTANH", "IDENTITY", "LEAKYRELU",
+                "RATIONALTANH", "RELU", "RRELU", "SIGMOID", "SOFTMAX", "SOFTPLUS", "SOFTSIGN",
+                "TANH", "RECTIFIEDTANH", "SELU"};
+
+//        for (String activation : activations){
+//            for (WeightInit weightInit : weightInits){
+//                long startTime = System.currentTimeMillis();
+//                runNetwork(activation, weightInit);
+//                long estimatedTime = System.currentTimeMillis() - startTime;
+//                System.out.println("===================\n"+estimatedTime+"\n=================");
+//            }
+//        }
+
+
+        runNetwork("IDENTITY", WeightInit.XAVIER_FAN_IN);
+        writeResultsToFile(results);
+    }
+
+    private static void writeResultsToFile (HashMap<String, List<Double>> input){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Activation,WeightInit,F1,precision,recall\n");
+        for (String key : input.keySet()){
+            String [] temp = key.split("  ");
+            sb.append(temp[0]+","+temp[1]+",");
+            for (Double aDouble : input.get(key)){
+                sb.append(aDouble+",");
+            }
+            sb.append("\n");
+        }
+        Helper helper = new Helper();
+        helper.writeFileBytes(HOME_PATH+"stats.csv", sb.toString());
+    }
+
+    public static void runNetwork (String activation, WeightInit weightInit) throws IOException, InterruptedException {
         int batchSize = 1000;
-//        final String filenameTrain  = new ClassPathResource("temp/trainingsSetSmall.csv").getFile().getPath();
-//        final String filenameTest  = new ClassPathResource("temp/testSetSmall.csv").getFile().getPath();
+//        final String filenameTrain  = new ClassPathResource("trainingsSetSmall.csv").getFile().getPath();
+//        final String filenameTest  = new ClassPathResource("testSetSmall.csv").getFile().getPath();
 
         final String filenameTrain  = new ClassPathResource("trainingsSet.csv").getFile().getPath();
         final String filenameTest  = new ClassPathResource("testSet.csv").getFile().getPath();
 
         RecordReader rr = new CSVRecordReader();
         rr.initialize(new FileSplit(new File(filenameTrain)));
-        DataSetIterator trainIter = new RecordReaderDataSetIterator(rr,batchSize,0,2);
-
+        DataSetIterator trainIter = new RecordReaderDataSetIterator(rr,batchSize,0,NUMFINALCLASSES);
         RecordReader rrTest = new CSVRecordReader();
         rrTest.initialize(new FileSplit(new File(filenameTest)));
-        DataSetIterator testIter = new RecordReaderDataSetIterator(rrTest,batchSize,0,2);
+        DataSetIterator testIter = new RecordReaderDataSetIterator(rrTest,batchSize,0,NUMFINALCLASSES);
+        MultiLayerNetwork net = new MultiLayerNetwork(getFeedForewardConf(activation, weightInit));
 
-        // Set up network configuration
-        NeuralNetConfiguration.Builder builder = new NeuralNetConfiguration.Builder();
-        builder.iterations(5000);
-        builder.learningRate(0.01);
-        builder.seed(123);
-        builder.useDropConnect(false);
-        builder.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT);
-        builder.biasInit(0);
-        builder.miniBatch(false);
-
-        // create a multilayer network with 2 layers (including the output
-        // layer, excluding the input payer)
-        ListBuilder listBuilder = builder.list();
-        DenseLayer.Builder hiddenLayerBuilder = new DenseLayer.Builder();
-        // two input connections - simultaneously defines the number of input
-        // neurons, because it's the first non-input-layer
-        hiddenLayerBuilder.nIn(5);
-        // number of outgooing connections, nOut simultaneously defines the
-        // number of neurons in this layer
-        hiddenLayerBuilder.nOut(20);
-        // put the output through the sigmoid function, to cap the output
-        // valuebetween 0 and 1
-        hiddenLayerBuilder.activation(Activation.SIGMOID);
-        // random initialize weights with values between 0 and 1
-        hiddenLayerBuilder.weightInit(WeightInit.DISTRIBUTION);
-        hiddenLayerBuilder.dist(new UniformDistribution(0, 1));
-        // build and set as layer 0
-        listBuilder.layer(0, hiddenLayerBuilder.build());
-
-        Builder outputLayerBuilder = new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD);
-        outputLayerBuilder.nIn(20);
-        outputLayerBuilder.nOut(2);
-        outputLayerBuilder.activation(Activation.SOFTMAX);
-        outputLayerBuilder.weightInit(WeightInit.DISTRIBUTION);
-        outputLayerBuilder.dist(new UniformDistribution(0, 1));
-        listBuilder.layer(1, outputLayerBuilder.build());
-
-        listBuilder.pretrain(false);
-        listBuilder.backprop(true);
-        // build and init the network, will check if everything is configured
-        // correct
-        MultiLayerConfiguration conf = listBuilder.build();
-        MultiLayerNetwork net = new MultiLayerNetwork(conf);
         net.init();
         // add an listener which outputs the error every 100 parameter updates
         net.setListeners(new ScoreIterationListener(100));
@@ -118,8 +124,38 @@ public class NeuralNetwork {
         //Print the evaluation statistics
         System.out.println(eval.stats());
         //Save the network
-        File saveLocation = new File("src/main/resources/model/trainedNetwork.zip");
+        File saveLocation = new File(HOME_PATH+"trainedNetwork.zip");
         boolean saveUpdater = true;
         ModelSerializer.writeModel(net,saveLocation,saveUpdater);
+    }
+
+    private static MultiLayerConfiguration getFeedForewardConf (String activation, WeightInit weightInit){
+        MultiLayerConfiguration configuration = new NeuralNetConfiguration.Builder()
+                .seed(123)
+                .iterations(10000)
+                .activation(Activation.fromString(activation))
+                .weightInit(weightInit)
+                .learningRate(0.001)
+                .regularization(true).l2(1e-4)
+                .list()
+                .layer(0, new DenseLayer.Builder().nIn(NUMFEATUREVECTORSIZE).nOut(NUMHIDDENNODES)
+                        .build())
+                .layer(1, new DenseLayer.Builder().nIn(NUMHIDDENNODES).nOut(NUMHIDDENNODES)
+                        .build())
+                .layer(2, new DenseLayer.Builder().nIn(NUMHIDDENNODES).nOut(NUMHIDDENNODES)
+                        .build())
+                .layer(3, new DenseLayer.Builder().nIn(NUMHIDDENNODES).nOut(NUMHIDDENNODES)
+                        .build())
+                .layer(4, new DenseLayer.Builder().nIn(NUMHIDDENNODES).nOut(NUMHIDDENNODES)
+                        .build())
+                .layer(5, new DenseLayer.Builder().nIn(NUMHIDDENNODES).nOut(NUMHIDDENNODES)
+                        .build())
+                .layer(6, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .activation(Activation.SOFTMAX)
+                        .nIn(NUMHIDDENNODES).nOut(NUMFINALCLASSES).build())
+                .backprop(true).pretrain(false)
+                .build();
+
+        return configuration;
     }
 }
